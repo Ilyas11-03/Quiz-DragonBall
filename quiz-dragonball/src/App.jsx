2add7d9b1afd78366309af73c37c92555fc2fbb0
Ilyas11-03 <ilyas.aboulkassim@gmail.com>
2025-05-23 15:30:37 +0000

    this is last commit , well maybe

--- quiz-dragonball/src/App.jsx
+++ quiz-dragonball/src/App.jsx
@@ -6,13 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 function App() {
   
   return (
-    
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/quiz" element={<Quiz />} />
-      </Routes>
-    </Router>
+    <Quiz/>
 
   )
 }
