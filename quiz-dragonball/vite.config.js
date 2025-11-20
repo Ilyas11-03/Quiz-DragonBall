3b34a650ac6aded488001448360d8dbf5c6ec53f
Ilyas11-03 <ilyas.aboulkassim@gmail.com>
2025-11-20 15:50:57 +0100

    this one

--- quiz-dragonball/vite.config.js
+++ quiz-dragonball/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/quiz-dragonball/', // This is the base URL for the project
+  // base: '/quiz-dragonball/', // This is the base URL for the project
 })
