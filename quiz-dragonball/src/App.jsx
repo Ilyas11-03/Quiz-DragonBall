b7157dcbdbd782f145bb9e739d45b2d8e4d7dba0
Ilyas11-03 <ilyas.aboulkassim@gmail.com>
2025-11-20 15:41:55 +0100

    or this one

--- quiz-dragonball/src/App.jsx
+++ quiz-dragonball/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Quiz from './Component/Quiz/Quiz.jsx'
 import Home from './Component/Quiz/Home.jsx';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 
 function App() {
   
